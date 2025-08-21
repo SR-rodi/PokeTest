a3d17bbb5459bca2007beddb2ec0f7a18f9597c6
RodinSr <rodin_sr@mail.ru>
2025-08-22 01:17:27 +0300

    add pul to refresh

--- app/src/main/java/ru/sr/poketest/presentation/screen/home/PokeListScreen.kt
+++ app/src/main/java/ru/sr/poketest/presentation/screen/home/PokeListScreen.kt
@@ -13,7 +13,15 @@ import androidx.compose.foundation.lazy.grid.GridItemSpan
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.pulltorefresh.PullToRefreshBox
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
@@ -31,62 +39,112 @@ import ru.sr.poketest.domain.model.getColor
 import ru.sr.poketest.presentation.screen.home.model.PokemonVO
 import ru.sr.poketest.presentation.uiKit.button.CircleIconButton
 import ru.sr.poketest.presentation.uiKit.commonComponent.PokemonCard
+import ru.sr.poketest.presentation.uiKit.defaultPage.DefaultLoadingPage
 import ru.sr.poketest.presentation.uiKit.progress.DefaultErrorPage
 import ru.sr.poketest.presentation.uiKit.progress.DefaultProgressIndicator
 import ru.sr.poketest.presentation.uiKit.theme.PokeTheme
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun PokeListScreen(
     pagingItems: LazyPagingItems<PokemonVO>,
     onSearchClick: () -> Unit
 ) {
 
-    Box(Modifier.fillMaxSize()) {
+    var isRefreshing by remember {
+        mutableStateOf(false)
+    }
 
-        LazyVerticalGrid(
-            modifier = Modifier.fillMaxWidth(),
-            contentPadding = PaddingValues(horizontal = 16.dp),
-            columns = GridCells.Fixed(2),
-            horizontalArrangement = Arrangement.spacedBy(16.dp),
-            verticalArrangement = Arrangement.spacedBy(16.dp)
+    PullToRefreshBox(
+        isRefreshing = isRefreshing,
+        onRefresh = {
+            isRefreshing = true
+            pagingItems.refresh()
+        }) {
 
-        ) {
-            item(span = { GridItemSpan(2) }) {
-                Image(
-                    modifier = Modifier
-                        .padding(16.dp)
-                        .heightIn(max = 100.dp),
-                    painter = painterResource(R.drawable.pokemon_text_logo),
-                    contentDescription = "pokemon text logo",
-                )
-            }
+        Box(Modifier.fillMaxSize()) {
 
-            items(pagingItems.itemCount) { index ->
+            LazyVerticalGrid(
+                modifier = Modifier.fillMaxWidth(),
+                contentPadding = PaddingValues(horizontal = 16.dp),
+                columns = GridCells.Fixed(2),
+                horizontalArrangement = Arrangement.spacedBy(16.dp),
+                verticalArrangement = Arrangement.spacedBy(16.dp)
+
+            ) {
+                item(span = { GridItemSpan(2) }) {
+                    Image(
+                        modifier = Modifier
+                            .padding(16.dp)
+                            .heightIn(max = 100.dp),
+                        painter = painterResource(R.drawable.pokemon_text_logo),
+                        contentDescription = "pokemon text logo",
+                    )
+                }
+
                 when (pagingItems.loadState.refresh) {
                     is LoadState.Error -> {
-                        DefaultErrorPage(
-                            title = stringResource(R.string.pokemon_list_error_title),
-                            subtitle = stringResource(R.string.pokemon_list_error_subtitle)
-                        )
-                    }
-
-                    LoadState.Loading -> {
-                        DefaultProgressIndicator()
-                    }
-
-                    is LoadState.NotLoading -> {
-                        pagingItems[index]?.let { pokemon ->
-                            PokemonCard(
-                                name = pokemon.name,
-                                color = pokemon.color.getColor(),
-                                imageUrl = pokemon.imageUrl
+                        item {
+                            DefaultErrorPage(
+                                title = stringResource(R.string.pokemon_list_error_title),
+                                subtitle = stringResource(R.string.pokemon_list_error_subtitle)
                             )
                         }
                     }
+
+                    LoadState.Loading -> {
+                        item(span = { GridItemSpan(2) }) {
+                            DefaultLoadingPage()
+                        }
+                    }
+
+                    is LoadState.NotLoading -> {
+                        isRefreshing = false
+                        items(pagingItems.itemCount) { index ->
+                            pagingItems[index]?.let { pokemon ->
+                                PokemonCard(
+                                    name = pokemon.name,
+                                    color = pokemon.color.getColor(),
+                                    imageUrl = pokemon.imageUrl
+                                )
+                            }
+                        }
+                    }
+                }
+
+                when (pagingItems.loadState.append) {
+                    LoadState.Loading -> {
+                        item(span = { GridItemSpan(2) }) {
+                            Box(
+                                Modifier
+                                    .fillMaxWidth()
+                                    .padding(vertical = 16.dp),
+                                contentAlignment = Alignment.Center
+                            ) {
+                                DefaultProgressIndicator()
+                            }
+                        }
+                    }
+
+                    is LoadState.Error -> {
+                        item(span = { GridItemSpan(2) }) {
+                            Box(
+                                Modifier
+                                    .fillMaxWidth()
+                                    .padding(16.dp),
+                                contentAlignment = Alignment.Center
+                            ) {
+                                TextButton(onClick = { pagingItems.retry() }) {
+                                    Text(text = stringResource(R.string.pokemon_error_paging))
+                                }
+                            }
+                        }
+                    }
+
+                    else -> {}
                 }
             }
         }
-
         CircleIconButton(
             modifier = Modifier
                 .padding(28.dp)
@@ -96,6 +154,7 @@ fun PokeListScreen(
         )
     }
 }
+
 
 @Preview(showSystemUi = false, showBackground = true)
 @Composable
